83066175dacf5d124f0cd27ef03e9d60eea49cab
MD ASHIK MAHMUD <ashik.swfu@outlook.com>
2022-07-05 23:09:52 +0800

    some extra things

--- src/Components/Body/NavigationBar/NavigationBar.js
+++ src/Components/Body/NavigationBar/NavigationBar.js
@@ -2,6 +2,10 @@ import React from 'react';
 import Layout from '../../Layout/Layout';
 import MenuIcon from '@mui/icons-material/Menu';
 import styles from './NavigationBar.module.css';
+import nav1 from '../../../images/nav-1.png';
+import nav1_responsive from '../../../images/nav-1-responsive.png';
+import nav2 from '../../../images/nav-2.png';
+import nav2_responsive from '../../../images/nav-2-responsive.png';
 
 const NavigationBar = () => {
     return (
@@ -10,23 +14,25 @@ const NavigationBar = () => {
                 <h2>navigation bar</h2>
             </div>
             <section className='section'>
-                <nav className={styles.nav}>
-                    <input type="checkbox" id='check' className={styles.check} />
-                    <label for="check" className={styles.checkbtn}>
-                        <MenuIcon/>
-                    </label>
+                <h3>nav-1</h3>
+                <img src={nav1} alt="nav" className={styles.nav1}/>
+                <h3>nav-1 responsive</h3>
+                <img src={nav1_responsive} alt="nav" className={styles.nav1_responsive}/>
+                
+                <br />
+                <a href="" className='codeAndSitebtn' >code</a> 
+                <a href="" className='codeAndSitebtn'>visit site</a>
+            </section>
 
-                    <label for="" className={styles.logo}>Nav-1</label>
-
-                    <ul>
-                        <li><a href="#" class="active">Home</a></li>
-                        <li><a href="#">About</a></li>
-                        <li><a href="#">Services</a></li>
-                        <li><a href="#">Contact</a></li>
-                        <li><a href="#">Feedback</a></li>
-                    </ul>
-                </nav>
-                <section></section>
+            <section className='section'>
+                <h3>nav-2</h3>
+                <img src={nav2} alt="nav" className={styles.nav1}/>
+                <h3>nav-2 responsive</h3>
+                <img src={nav2_responsive} alt="nav" className={styles.nav1_responsive}/>
+                
+                <br />
+                <a href="" className='codeAndSitebtn' >code</a> 
+                <a href="" className='codeAndSitebtn'>visit site</a>
             </section>
         </Layout>
     );
